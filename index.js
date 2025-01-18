9444b5c78ad76d166a0919a27f7269bb6a74bad9
M A Murad <72880355+mozaddedalfeshani@users.noreply.github.com>
2025-01-18 18:30:21 +0600

    update payment intent creation to use price from request body

--- index.js
+++ index.js
@@ -255,9 +255,12 @@ app.get("/client/userStories/:email", async (req, res) => {
 app.post("/create-payment-intent", async (req, res) => {
   const { items } = req.body;
 
+  const { price } = req.body;
+  const amount = parseInt(price * 100);
+
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
+    amount: amount,
     currency: "usd",
     payment_method_types: ["card"],
   });
